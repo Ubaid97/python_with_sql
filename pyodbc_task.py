54e82fab9c6cbdffef4ba4a2891893ddb9cf4e7c
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-11 16:56:25 +0000

    implemented function to display table

--- pyodbc_task.py
+++ pyodbc_task.py
@@ -10,9 +10,6 @@ class PyodbcTask:
         self.northwind_connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password)
         # server name - database name - username and password is required to connect to pyodbc
         self.cursor = self.northwind_connection.cursor()
-        # self.cursor.execute("SELECT @@VERSION")
-        # self.row = self.cursor.fetchone()
-        # print(self.row)
         self.table_name = ""
         self.column_name = []
         self.column_datatype = []
@@ -31,16 +28,18 @@ class PyodbcTask:
             f"CREATE TABLE {self.table_name} ( {self.column_name[0]} {self.column_datatype[0]}, {self.column_name[1]} {self.column_datatype[1]} )")
         return table
 
+
     def input_data(self):
         column_1 = input(f"Would you like to input data for {self.column_name[0]}? Y/N ").upper()
         if column_1 == "Y":
             column_1_data = input("Please enter the data you'd like to add: ")
-            table_data = self.cursor.execute(f"INSERT INTO {self.table_name} ({self.column_name[0]}) VALUES ({column_1_data});")
 
-        column_2 = input(f"Would you like to input data for {self.column_name[1]}? Y/N" ).upper()
+        column_2 = input(f"Would you like to input data for {self.column_name[1]}? Y/N ").upper()
         if column_2 == "Y":
-            column_2_data = input("Please enter the data you'd like to add:")
-            table_data_2 = self.cursor.execute(f"INSERT INTO {self.table_name} ({self.column_name[1]}) VALUES ({column_2_data});")
+            column_2_data = input("Please enter the data you'd like to add: ")
+
+            table_data = self.cursor.execute(f"INSERT INTO {self.table_name} ({self.column_name[0]}, {self.column_name[1]}) VALUES ('{column_1_data}', '{column_2_data}')")
+
 
     def display_table(self):
         table_rows = self.cursor.execute(f"SELECT * FROM {self.table_name}").fetchall()
@@ -51,3 +50,4 @@ class PyodbcTask:
 test_run = PyodbcTask()
 test_run.create_table()
 test_run.input_data()
+test_run.display_table()
