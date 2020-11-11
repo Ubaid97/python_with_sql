e58678d0754aed1cd2dcea3b8ab9c6ca2f5d323d
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-11 14:29:27 +0000

    updated README

--- README.md
+++ README.md
@@ -5,3 +5,33 @@
 **some functions that we can use to interact with SQL data**
 - install pyodbc using ```pip install pyodbc```
 - Set up pyobdc connection: ```import pyodbc```
+- install odbc driver from: https://docs.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver15
+
+- establish a connection:
+```python
+server = "databases1.spartaglobal.academy"
+database = "Northwind"
+username = "SA"
+password = "Passw0rd2018"
+northwind_connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+```
+- server name - database name - username and password is required to connect to pyodbc
+verify connection:
+```python
+cursor = northwind_connection.cursor()
+# cursor is location of your mouse/current path
+cursor.execute("SELECT @@VERSION")
+select the version of current DB
+row = cursor.fetchone()
+print(row)
+```
+- get data from a table in the DB:
+```python
+cust_row = cursor.execute("SELECT * FROM Customers;").fetchall()
+```
+- using fetchall() method we can get all the data in Customers table
+- run queries:
+```python
+for records in cust_row:
+    print(records)
+```
--- python_sql.py
+++ python_sql.py
@@ -1,1 +1,39 @@
+# This lesson will include connection to our SQL DB from Python using PYODBC
+
+# pyodbc driver from microsoft helps us to connect to SQL instance
+# we will connect to our Northwind DB which you have already used in SQL week
+
 import pyodbc
+
+server = "databases1.spartaglobal.academy"
+database = "Northwind"
+username = "SA"
+password = "Passw0rd2018"
+northwind_connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+# server name - database name - username and password is required to connect to pyodbc
+cursor = northwind_connection.cursor()
+# cursor is location of your mouse/current path
+# cursor.execute("SELECT @@VERSION")
+# select the version of current DB
+# row = cursor.fetchone()
+# print(row)
+
+# In our DB, we have a table Customers with customers' data
+cust_row = cursor.execute("SELECT * FROM Customers;").fetchall()
+# using fetchall() method we can get all the data in Customers table
+# print(cust_row)
+for records in cust_row:
+    print(records)
+
+# Products table
+# product_rows = cursor.execute("SELECT * FROM Products").fetchall()
+# for pr_records in product_rows:
+#     print(pr_records.UnitPrice)
+
+product_rows = cursor.execute("SELECT * FROM Products").fetchall()
+# loop ensures we only iterate through data as long as data is available
+while True:
+    records = product_rows.fetchone()
+    if records is None:
+        break
+    print(records.UnitPrice)
